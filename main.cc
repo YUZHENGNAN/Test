a129189f264605efda5be7cfb9df0a7487f8887a
yuzhengnan <yzn279859701@icloud.com>
2019-05-24 16:41:29 +0900

    turn to opencv project

--- main.cc
+++ main.cc
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 using namespace std;
+using namespace cv;
+
 int main(int argc, char* argv[])
 {
 	cout<<"hello world"<<endl;
-	cout<<"this is a git test file"<<endl;
-	cout<<"test"<<endl;
+	Mat m = imread("../opencv-logo.png");
+	imshow("test", m);
+	waitKey(0);
+
+	enum Test{
+		T1,
+		T2,
+	};
+
+	vector<int> test(T2);
+	cout<<T2<<endl;
+	cout<<test.size()<<endl;
+	
+
 	return 0;
 }
